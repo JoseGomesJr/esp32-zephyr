b893279e07764eae51f81388a643a0d376bcc081
JoseGomes <59032543+JoseGomesJr@users.noreply.github.com>
2023-08-30 15:13:07 -0300

    Teste Inicial

---
+++ esp32_zephyr/build/zephyr/zephyr.dts.d
@@ -0,0 +1,18 @@
+empty_file.o: C:/Users/josej/zephyrproject/zephyr/misc/empty_file.c \
+ C:/Users/josej/zephyrproject/zephyr/boards/xtensa/esp32/esp32.dts \
+ C:/Users/josej/zephyrproject/zephyr/dts/xtensa/espressif/esp32.dtsi \
+ C:/Users/josej/zephyrproject/zephyr/dts/common/mem.h \
+ C:/Users/josej/zephyrproject/zephyr/dts/xtensa/xtensa.dtsi \
+ C:/Users/josej/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/esp32_clock.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/interrupt-controller/esp-xtensa-intmux.h \
+ C:/Users/josej/zephyrproject/modules/hal/espressif/include/dt-bindings/pinctrl/esp32-pinctrl.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ C:/Users/josej/zephyrproject/zephyr/boards/xtensa/esp32/esp32-pinctrl.dtsi \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/esp-pinctrl-common.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/sys/util_macro.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/sys/util_internal.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/sys/util_loops.h \
+ C:/Users/josej/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/esp32-gpio-sigmap.h
